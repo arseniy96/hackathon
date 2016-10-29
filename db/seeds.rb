9dad460331a980193f0861162938094c810d604c
arseniy96 <arsenzhar@gmail.com>
2016-10-29 15:36:57 +0300

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 User.create!
+Task.create!(name: 'First task', description: 'Lorem ipsum', date: Time.now + 60000, complete: false, user_id: 1)
