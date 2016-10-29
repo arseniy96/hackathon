0527ead0c33018cd5d430178c922414a059dd35e
arseniy96 <arsenzhar@gmail.com>
2016-10-29 17:46:16 +0300

    Delete completed tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,6 +5,7 @@ class TasksController < ApplicationController
   # GET /tasks.json
   def index
     @tasks = Task.where(user_id: 1)
+    delete_complited(@tasks)
 
     render json: @tasks
   end
@@ -31,10 +32,16 @@ class TasksController < ApplicationController
   # PATCH/PUT /tasks/1
   # PATCH/PUT /tasks/1.json
   def update
+    @tasks = Task.all
     @task = Task.find(params[:id])
 
     if @task.update(task_params)
-      render json: @task
+      if @task.complete == true
+        @task.destroy
+        render json: @tasks
+      else
+        render json: @task
+      end
     else
       render json: @task.errors, status: :unprocessable_entity
     end
@@ -48,6 +55,14 @@ class TasksController < ApplicationController
     @tasks = Task.all
 
     render json: @tasks
+  end
+
+  def delete_complited(tasks)
+    tasks.each do |task|
+      if task.complete == true
+        task.destroy
+      end
+    end
   end
 
   private
