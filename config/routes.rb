5f66dcb832d7ba9888cc697de2bacd65ecfd8bf4
arseniy96 <arsenzhar@gmail.com>
2016-10-29 16:04:58 +0300

    Add prefix /api to URL

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :tasks, except: [:new, :edit]
-  resources :serials, except: [:new, :edit]
-  resources :users, except: [:new, :edit]
+  scope '/api' do
+    resources :tasks, except: [:new, :edit]
+    resources :serials, except: [:new, :edit]
+    resources :users, except: [:new, :edit]
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
