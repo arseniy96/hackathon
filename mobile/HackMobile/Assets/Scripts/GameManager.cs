daa19d9838e61940855e63f95a59fc97351a5302
Shormy <noxormy@gmail.ru>
2016-10-29 16:32:31 +0300

    update create panels

---
+++ mobile/HackMobile/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject panel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void CreateNewObject(){
+		Debug.Log("new obhewct");
+		GameObject	
+	
+	}
+}
