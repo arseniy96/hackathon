cf30b98777da042b34bc6f29af54e6fb60ccd462
= <alexander.goncharof@gmail.com>
2016-10-29 17:11:12 +0300

    add net init

---
+++ mobile/HackMobile/Assets/Scripts/NetworkConnetor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkConnetor : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(GetTasksList ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public IEnumerator GetTasksList(){
+		WWW request = new WWW ("http://spoiler-motivator.herokuapp.com/api/tasks");
+		yield return request;
+		if (request.error == null) {
+			Debug.Log (request.data);
+		} else {
+			Debug.Log (request.error);
+		}
+	}
+
+}
