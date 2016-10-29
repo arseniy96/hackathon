c10aadf74937a8375a352cabb130328b1aae88cf
Shormy <noxormy@gmail.ru>
2016-10-29 18:30:40 +0300

    update rotation camera

--- mobile/HackMobile/Assets/Scripts/GameManager.cs
+++ mobile/HackMobile/Assets/Scripts/GameManager.cs
@@ -5,6 +5,7 @@ public class GameManager : MonoBehaviour {
 
 	public GameObject panel;
 	public GameObject Canvas;
+	public GameObject MainCamera;
 	
 	private int operation;
 	// Use this for initialization
@@ -26,7 +27,15 @@ public class GameManager : MonoBehaviour {
 		Vector3 pos = new Vector3 (540, y, 0);	
 		
 		GameObject newPanel = Instantiate(panel, pos, Quaternion.identity) as GameObject;
+		
 		newPanel.transform.SetParent(Canvas.transform, false);
+		
+		
 
 	}
+	
+	public void Camera_rotation() {
+		MainCamera.transform.position = new Vector3(5.56f, 0.0f, -10.0f);
+	}
 }
+
