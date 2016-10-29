e35a836f88559b1e0f9316d630feb218e27dcc12
arseniy96 <arsenzhar@gmail.com>
2016-10-29 12:31:28 +0300

    Create App with PostgreSQL

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+Motivator::Application.config.secret_token = '77e23e9af3df3a7c8a2066c9bd59555227584db762ed28e544595d32c4d69fe51531ad3217385334d7b584cb2ab15ea64fe25350591a8f127b69f55725ace538'
