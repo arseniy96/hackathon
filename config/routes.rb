7457fbb83ca2dce0e46839c34426382dbf9a239e
arseniy96 <arsenzhar@gmail.com>
2016-10-30 01:10:18 +0300

    Bug fix

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
     resources :tasks, except: [:new, :edit] do
       get 'complete', on: :member
     end
-    resources :serials, except: [:new, :edit]
-    resources :users, except: [:new, :edit] do
+    resources :serials, except: [:new, :edit] do
       get 'destroy_serial', on: :member
     end
+    resources :users, except: [:new, :edit]
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
