a30995f0900ca90ea9c4409c21d59a1579823353
arseniy96 <arsenzhar@gmail.com>
2016-10-30 03:24:40 +0300

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,11 @@ User.create!
 Task.create!(name: 'First task', description: 'Lorem ipsum', date: Time.now + 60000, complete: false, user_id: 1)
 Serial.create!(name: 'Game of Thrones', description: 'Blabla')
 Serial.create!(name: 'The Bing Bang Theory', description: 'Lorem ipsum')
-Serial.create!(name: 'Peaky Bblinders', description: '132')
+Serial.create!(name: 'Peaky Blinders', description: '132')
 Serial.create!(name: 'Walking Dead', description: '132')
 Serial.create!(name: 'Preacher', description: '132')
 Serial.create!(name: 'Star Wars', description: '132')
-
+Spoiler.create!(text: 'John Snow is alive', serial_id: 1)
+Spoiler.create!(text: 'Rob Stark was dead', serial_id: 1)
+Spoiler.create!(text: 'Margaery Tyrell was dead', serial_id: 1)
+Spoiler.create!(text: "Darth Vader was Luke's father", serial_id: 6)
